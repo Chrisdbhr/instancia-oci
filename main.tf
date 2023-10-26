9200abf7e8f5917ff8652dae295ac5970be9ae4b
Chris <19819051+Chrisdbhr@users.noreply.github.com>
2023-10-26 04:20:32 +0000

    terraform

--- main.tf
+++ main.tf
@@ -38,19 +38,7 @@ resource "oci_core_instance" "generated_oci_core_instance" {
 		}
 		plugins_config {
 			desired_state = "DISABLED"
-			name = "Oracle Java Management Service"
-		}
-		plugins_config {
-			desired_state = "ENABLED"
-			name = "OS Management Service Agent"
-		}
-		plugins_config {
-			desired_state = "DISABLED"
 			name = "Compute RDMA GPU Monitoring"
-		}
-		plugins_config {
-			desired_state = "ENABLED"
-			name = "Compute Instance Run Command"
 		}
 		plugins_config {
 			desired_state = "ENABLED"
@@ -77,21 +65,21 @@ resource "oci_core_instance" "generated_oci_core_instance" {
 		is_live_migration_preferred = "true"
 		recovery_action = "RESTORE_INSTANCE"
 	}
-	availability_domain = "tJil:SA-SAOPAULO-1-AD-1"
-	compartment_id = "ocid1.tenancy.oc1..aaaaaaaappgufk7p6rh3m6nartt2wajmmlsl4kudupriaxliwfzrjwp2wwwq"
+	availability_domain = "NXhS:SA-SAOPAULO-1-AD-1"
+	compartment_id = "ocid1.tenancy.oc1..aaaaaaaauvulx2dk6ypl4wjafr67jz2vkbqi75g3se53icechv5e5a52s6sa"
 	create_vnic_details {
 		assign_ipv6ip = "false"
 		assign_private_dns_record = "true"
 		assign_public_ip = "true"
-		subnet_id = "ocid1.subnet.oc1.sa-saopaulo-1.aaaaaaaa22sn2t72t25mlcgohbka3ru3mgooeky5xglwjtu46ra3mtfsvwcq"
+		subnet_id = "ocid1.subnet.oc1.sa-saopaulo-1.aaaaaaaaoqimg5c6leh4hgdvrv65fp7xi6bxnudte2t5coax5sqjbw4uqlqq"
 	}
-	display_name = "self-hosted"
+	display_name = "based"
 	instance_options {
 		are_legacy_imds_endpoints_disabled = "false"
 	}
 	is_pv_encryption_in_transit_enabled = "true"
 	metadata = {
-		"ssh_authorized_keys" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCysF/KFZmcUdhvmwwVCibjoNTCske1hsQpBNHsO491kWjyDoydNEYUYjDyJ0EUoYqdaFVFYakGAgQJAe7yUth4wprzQaMdzsIK46rAjhvl0E9fRT/gEXLEfH8lmwAT4B40yCxc+bXdx6r70Wf0+lvDzR9uxUtBESIGEWmeY+jpMhxzZfyhJ/bWwTFSUEDsVdkpVDex5yFSpzw1zq85iIWUenmF+F57QmKOXdpFEOU9jnlRbWDnZVHIvHBM+wtWV8d4J2IZXSwwh5mPUlU81g6Hu1dgwobok/J2TN21GhfRAL3Zf3gOoUnbe6S0kAB34QWLZ3qcgu8rMatOuA4YbsTJ ssh-key-2023-10-25"
+		"ssh_authorized_keys" = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5e04vGZEQ10tfDmC1a2pWnxlUBGzlNiP1FMSFKz6uGNGvO/SxFBU3fQtXEP5Cj1Fdj6/wMx9bGroVTFGt1d6CsTLGkNDaLS4HOIbbHpia9RMx1AVA77cRid1aoiOESqh8BiIZTYb9+gMjl8HRSVvp3DTdRcpz8qaZq4i6XLXAvKNFs1IXwHCVVKkjWQA0p37TvPiyQbZnSgSuf2VKQuFzUPhBM2SAIv01C4ScvkCDtfWoQNfP8IpiJgeLPA2BdGbvi1rKVO8PkPXgOE1rw0puIE0hg5YC3xAJBfHt6XD53WV2J2oDRu/lNezsfN7yClwCNGqaKu6UstTFbzV/GQDN ssh-key-2023-10-26"
 	}
 	shape = "VM.Standard.A1.Flex"
 	shape_config {
@@ -99,7 +87,7 @@ resource "oci_core_instance" "generated_oci_core_instance" {
 		ocpus = "4"
 	}
 	source_details {
-		source_id = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaaen5tmigeuccvhcon6saps4aty3l3yh4fhipwyys2q2o2jecl25ya"
+		source_id = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaakvw6ryd3n4rh4piiitvsmzuhxgyzs7urdexxuojifnunhjmiujbq"
 		source_type = "image"
 	}
 }
